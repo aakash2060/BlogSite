4f05571d40ced3efd85bd5e53b9d166ea727f22d
Aakash Poudel <aakash.poudel.900@gmail.com>
2024-06-19 15:48:27 -0500

    Revert "Blog site with Home, about and create blog"
    This reverts commit d9532c691c14327d16199bf6f661756c88552dda.

--- export/module.js
+++
@@ -1,3 +0,0 @@
-const people =['aakash','hari', 'shaun', 'reci']
-const age =[20, 20, 30, 40]
-module.exports = {people, age}; // helps to export to another file
--- export/people.js
+++
@@ -1,7 +0,0 @@
-const { people, age } = require("./module");
-console.log(people, age);
-
-
-// about os > preconfigured file
-const os = require('os');
-console.log(os.platform(), os.homedir())
